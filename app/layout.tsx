15bbbc935e1b5cba205b24f0c7ce57f6005e4004
harshit-agarwal-7 <agarwal.harshit117@gmail.com>
2026-01-30 14:13:26 +0530

    Update OpenGraph metadata to include twitter info and fix og-image location

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,22 +13,31 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Consumer Price Index - India",
-  description: "Visualization tool for the consumer price index in Inida.",
+  metadataBase: new URL("https://consumer-price-index-green.vercel.app"),
+  title: "Consumer Price Index – India",
+  description: "Visualization tool for the consumer price index in India.",
   openGraph: {
     title: "Consumer Price Index Visualizer",
     description: "Explore CPI trends across India",
     url: "https://consumer-price-index-green.vercel.app",
+    siteName: "Consumer Price Index – India",
+    type: "website",
     images: [
       {
-        url: "https://consumer-price-index-green.vercel.app/docs/screenshots/og-image.jpg",
+        url: "/og-image.jpg",
         width: 1200,
         height: 630,
+        alt: "Consumer Price Index visualizer for India",
       },
     ],
-},
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Consumer Price Index Visualizer",
+    description: "Explore CPI trends across India",
+    images: ["/docs/screenshots/og-image.jpg"],
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
